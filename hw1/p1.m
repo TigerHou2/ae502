dfd5949b0c22282cc3c130c20f5f334f5d7c0e35
TigerHou2 <44105132+TigerHou2@users.noreply.github.com>
2021-03-01 21:41:39 -0600

    Final submission for Project 1.

--- hw1/p1.m
+++ hw1/p1.m
@@ -21,9 +21,7 @@ x1 = -alpha*R; % construct the system with barycenter at the origin
 x2 =   beta*R;
 
 w = sqrt(G*(m1+m2)/R^3);
-T = 80;
 dt = 0.001;
-steps = ceil(T/dt);
 
 % Lagrange points
 % https://ocw.mit.edu/courses/aeronautics-and-astronautics/16-07-dynamics-fall-2009/lecture-notes/MIT16_07F09_Lec18.pdf
@@ -34,11 +32,23 @@ L4 = [  0.5+x1,  0.5*sqrt(3), 0];
 L5 = [  0.5+x1, -0.5*sqrt(3), 0];
 
 % u0 = [ 0.5,  0.5,  0.0,  1.0,  0.5,  0.0]';
-u0 = [  L1 + [-4e-3,0,0], 0, 0, 0; ... % L1
-        L2 + [-3.7e-3,0,0], 0, 0, 0; ... % L2
-        L3 + [0,-1e-3,0], 0, 0, 0; ... % L3
-        L4 + [0,0.01,0], 0, 0, 0; ... % L4
-        L5 + [0,0.01,0], 0, 0, 0; ... % L5
+u1 = [...
+        L1 + [-4e-3,0,0], 0, 0, 0; ... L1 (unstable)
+        L4 + [0,0.01,0], 0, 0, 0; ... L4 (perturbed, stable)
+        L5 + [0,0.01,0], 0, 0, 0; ... L5 (perturbed, stable)
+        L1 + [7e-2,0,0], 0, -0.3, 0; ... lunar orbit
+        0.15, 0, 0, 0, 2.25, 0; ... LEO
+     ]';
+T1 = 40;
+
+u2 = [...
+        L2, 0, -2.046483231514e-2, 0; ... L2 (unstable, outbound)
+        L2, 0, -2.046483231515e-2, 0; ... L2 (unstable, inbound)
+        L3 + [0,2e-3,0], 0, 0, 0; ... L3 (perturbed, unstable)
+        L3 + [-1,0,0], 0, 2.7, 0; ... orbit around both bodies, small
+        L3 + [-2.8,0,0], 0, 4.31, 0; ... orbit around both bodies, large
+     ]';
+T2 = 35;
 %         0.5,  0.5*sqrt(3),  0.0,  0.0,  0.0,  0.0; ... % L4
 %         0.5, -0.5*sqrt(3),  0.0,  0.0,  0.0,  0.0; ... % L5
 %         (1+1e-6)*L3, 0.0,  0.0,  0.0,  -1.00002e-1,  0.0; ... % weird L3
@@ -49,7 +59,10 @@ u0 = [  L1 + [-4e-3,0,0], 0, 0, 0; ... % L1
 %         1.118824382902157, 0.0, ... % L2 from problem 3
 %         0.014654873101278, 0.0, ...
 %         0.180568501159703, 0.0 ...
-        ]'; 
+
+u0 = u2;
+T = T2;
+steps = ceil(T/dt);
 
 N = size(u0,2);
 uu = zeros(6,N,steps);
@@ -75,18 +88,20 @@ dat = permute(uu,[1,3,2]);
 
 figure(1)
 hold on
-scatter3(x1,0,0,'r','filled')
-scatter3(x2,0,0,'b','filled')
+M1 = scatter3(x1,0,0,'r','filled');
+M2 = scatter3(x2,0,0,'b','filled');
 for i = 1:N
-    plot3(dat(1,:,i),dat(2,:,i),dat(3,:,i))
+    plot3(dat(1,:,i),dat(2,:,i),dat(3,:,i),'LineWidth',1.25)
 end
 hold off
 xlabel('x')
 ylabel('y')
 zlabel('z')
+legend([M1,M2],'$M_1 = 1-\mu$','$M_2 = \mu$','Location','NorthWest')
 axis equal
 grid minor
 
+latexify(16,12,12)
 
 %% function definitions
 
--- hw1/pseudo_potential_deriv.m
+++ hw1/pseudo_potential_deriv.m
@@ -11,15 +11,6 @@ dudx = simplify(diff(U,x))
 dudy = simplify(diff(U,y))
 dudz = simplify(diff(U,z))
 
-% assuming w = 1
-% r1 = sqrt((x-x1)^2+y^2+z^2);
-% r2 = sqrt((x-x2)^2+y^2+z^2);
-% U = 1/2*(x^2+y^2) + m1/r1 + m2/r2;
-% 
-% dudx = simplify(diff(U,x))
-% dudy = simplify(diff(U,y))
-% dudz = simplify(diff(U,z))
-
 dudx_dx = simplify(diff(dudx,x))
 dudx_dy = simplify(diff(dudx,y))
 dudx_dz = simplify(diff(dudx,z))
